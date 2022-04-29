f72cc338ff2b94c2193df2134fb9c797b80acf11
zestaken <1224649136@qq.com>
2022-04-29 16:08:29 +0800

    1470数组重新排序完成

--- src/Array/Resort1470.cpp
+++ src/Array/Resort1470.cpp
@@ -6,11 +6,10 @@ class Resort {
 public:
     //传值只有指针，则必须将数组长度传入，光靠指针无法知道数组长度
     vector<int> shuffle(vector<int>& nums, int n) {
-        int n1 = n / 2;
         vector<int> res;
-        for(int i = 0; i < n1; i++) {
+        for(int i = 0; i < n; i++) {
             res.push_back(nums[i]);
-            res.push_back(nums[i + n1]);
+            res.push_back(nums[i + n]);
         }
         return res;
     }
@@ -19,7 +18,7 @@ public:
 int main() {
     vector<int> nums = {2, 5, 1, 3, 4, 7};
     Resort resort;
-    vector<int> res = resort.shuffle(nums, 6);
+    vector<int> res = resort.shuffle(nums, 3);
     for(int i = 0; i < nums.size(); i++) {
         cout << res[i] << endl;
     }
