f0c6e96b6311d9a89f9db532721888bd7ac6890b
zestaken <1224649136@qq.com>
2022-05-04 18:12:08 +0800

    左旋转字符串完成

--- src/Array/ReverseLeftWords.cpp
+++ src/Array/ReverseLeftWords.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Solution {
 public:
     string reverseLeftWords(string s, int n) {
-        for(int i = 0; i < n; i++) {
-            cout << s[i] << endl;
-        }
+        if(n == 0) return s;
+        char temp[s.length()];
+
+        for(int i = n; i < s.length(); i++) {
+            temp[i-n] = s[i];
     }
+        for(int i = 0; i < n; i++) {
+            temp[s.length() - n + i] = s[i];
+        }
+    string res;
+    for(int i = 0; i < s.length(); i++) {
+        res += temp[i];
+    }
+    return res;
+}
 };
 
 int main() {
     Solution solution;
-    string s = "test";
-    string s1 = solution.reverseLeftWords(s, s.length());
+    string s = "abcdefg";
+    string s1 = solution.reverseLeftWords(s, 2);
+    for(int i = 0; i < s1.length(); i++) {
+        cout << s1[i] << endl;
+    }
 }
--- src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    for(int i = 0; i < 5; i++) {
-        printf("hello%d\n", i);
-    }
-}
